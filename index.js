95ddecf55a2fcf020f1b3898b2e904fbf12351d7
Ngô Xuân Bách <thanbaiks@gmail.com>
2017-02-23 21:53:36 +0700

    Set greeting and getting started button

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('./lib/app.js');
 
+app.registerMenu();
 app.listen(5000, () => {
     console.log('Express application running at 0.0.0.0:%d [NODE_ENV: %s]', process.env.HTTP_PORT, process.env.NODE_ENV);
 });
