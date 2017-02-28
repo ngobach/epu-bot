3820894d97af9c323ea25b9bb709eb1527042275
Ngô Xuân Bách <thanbaiks@gmail.com>
2017-03-01 00:10:05 +0700

    Move to gulp builder

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const htmlmin = require('gulp-htmlmin');
+const replace = require('gulp-replace');
+
+
+gulp.task('scss', () => {
+    return gulp.src('public/src/style.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('html', () => {
+    return gulp.src('public/src/index.html')
+        .pipe(replace(':SITE_URL:', process.env.SITE_URL))
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            collapseInlineTagWhitespace: true,
+            collapseBooleanAttributes: true,
+            minifyJS: true,
+            removeComments: true
+        }))
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('public/src/style.scss', ['scss']);
+    gulp.watch('public/src/index.html', ['html']);
+});
+
+gulp.task('default', ['html', 'scss', 'watch']);
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+require('dotenv').config();
+
 const app = require('./lib/app.js');
 const listenPort = process.env.HTTP_PORT || 80;
 
-app.registerMenu();
+if (process.env.NODE_ENV == 'production') {
+    app.registerMenu();
+}
+
 app.listen(listenPort, () => {
     console.log('Express application running at 0.0.0.0:%d [NODE_ENV: %s]', listenPort, process.env.NODE_ENV);
 });
